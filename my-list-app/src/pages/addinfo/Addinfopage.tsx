32d948ef95cbffe8b2a5406d7a76c08f82a79d06
Nithin N <nithin.n@saarthi.ai>
2022-08-11 18:13:33 +0530

    v1.1

--- my-list-app/src/pages/addinfo/Addinfopage.tsx
+++ my-list-app/src/pages/addinfo/Addinfopage.tsx
@@ -200,9 +200,9 @@ export default function Addinfopage() {
   return (
     <motion.div
       className={styles.addinfo}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0 }}
       transition={{ duration: 1 }}
     >
       <div className={styles.wrapper}>
--- my-list-app/src/pages/userpage/Userpage.tsx
+++ my-list-app/src/pages/userpage/Userpage.tsx
@@ -15,9 +15,9 @@ export default function Userpage() {
   return (
     <motion.div
       className={styles.userpage}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0 }}
       transition={{ duration: 1 }}
     >
       <Themechanger></Themechanger>
